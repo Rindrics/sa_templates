4131063b03d9c2f5e7688031c74f3e57980baf33
Akira Hayashi <>
2018-06-19 22:58:01 +0900

    Complete Anchvy sample

--- typeset_plan2.sh
+++ typeset_plan2.sh
@@ -4,6 +4,11 @@ pbibtex ./fish1
 platex ./fish1
 platex ./fish1
 dvipdfmx ./fish1
+platex ./fish2
+pbibtex ./fish2
+platex ./fish2
+platex ./fish2
+dvipdfmx ./fish2
 platex ./master_plan2
 dvipdfmx ./master_plan2
 open -a Skim ./master_plan2.pdf
