187d9a1451a258303f2ba36dc9f0a26037ea55ba
Akira Hayashi <>
2018-06-19 20:44:51 +0900

    Rename master .tex file

--- typeset_plan1.sh
+++ typeset_plan1.sh
@@ -1,9 +1,8 @@
-platex ./_TypesetMe
+
+platex ./master_plan1
 pbibtex ./PF_Engraulis-japonicus_nonTAC_1_withB
 pbibtex ./TW_Engraulis-japonicus_nonTAC_1_withB
-platex ./_TypesetMe
-platex ./_TypesetMe
-dvipdfmx ./_TypesetMe
-open -a Skim ./_TypesetMe.pdf
-
-
+platex ./master_plan1
+platex ./master_plan1
+dvipdfmx ./master_plan1
+open -a Skim ./master_plan1.pdf
