8a3c5f4f54a9314660ddee39811e0ab20066eb90
Akira Hayashi <smxshxishxad@me.com>
2018-12-06 06:29:31 +0900

    Remove stockdir

--- Engraulis-japonicus/TW/_bk/R4TeX/ターミナルから一括実行.R
+++
@@ -1,3 +0,0 @@
-THISYEAR        <- 2016
-source("R4TeX/定数.R")
-source("R4TeX/ターミナルからTeX表に書き出し_過去5年間の資源量等.R")
--- Engraulis-japonicus/TW/_bk/R4TeX/定数.R
+++
@@ -1,6 +0,0 @@
-THISYEAR            <- 2016
-REVIEW_RANGE_yr     <- 5
-STOCK_ABB           <- "TW"
-STOCK_ABB_JP        <- "対馬暖流系群"
-SPCS_NAME           <- "Engraulis-japonicus"
-SPCS_NAME_JP        <- "カタクチイワシ"
